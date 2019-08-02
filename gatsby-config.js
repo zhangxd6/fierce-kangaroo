2ed7c9893905c9d5b3d85283eb336ad53b442962
Xiangdong <zhangxd6@hotmail.com>
2019-08-02 14:36:56 -0500

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,22 @@ module.exports = {
     pathPrefix: '/',
     siteMetadata: require('./site-metadata.json'),
     plugins: [
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                trackingId: "UA-42947794-1",
+                // Defines where to place the tracking script - `true` in the head and `false` in the body
+                head: false,
+                // Setting this parameter is optional
+                anonymize: true,
+                // Setting this parameter is also optional
+                respectDNT: true,
+                // Avoids sending pageview hits from custom paths
+                exclude: ["/preview/**", "/do-not-track/me/too/"],
+                // Delays sending pageview hits on route update (in milliseconds)
+                pageTransitionDelay: 0
+            },
+        },
         `gatsby-plugin-react-helmet`,
         `gatsby-source-data`,
         {
@@ -22,17 +38,18 @@ module.exports = {
             resolve: `gatsby-transformer-remark`,
             options: {
                 plugins: [
-                    {resolve: "gatsby-remark-relative-links",
-                    options: { 
-                        domainRegex: /\/posts\/*.md/,                     
-                    }
-                },`gatsby-remark-component`]
+                    {
+                        resolve: "gatsby-remark-relative-links",
+                        options: {
+                            domainRegex: /\/posts\/*.md/,
+                        }
+                    }, `gatsby-remark-component`]
             }
         },
         {
             resolve: `gatsby-remark-page-creator`,
             options: {
-                
+
             }
         },
         {
